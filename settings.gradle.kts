d3f6b4af1ac71d9f4f5e515a9077c92a0a05379a
f43nd1r <account+github@faendir.com>
2021-12-04 15:22:04 +0100

    add necessary repos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
 enableFeaturePreview("VERSION_CATALOGS")
 rootProject.name = "renovate-bot-multiple-prs-bug-reproducer"
 
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven { url = uri("https://repo.nokee.dev/release") }
+        maven { url = uri("https://repo.nokee.dev/snapshot") }
+    }
+}
+
